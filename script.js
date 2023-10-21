4f74a672071f61ccf079f02ab73e08ff09d02c4a
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-10-21 23:09:29 +0700

    Create player factory function with name and marker properties and methods to get and set the property. Add object to create and store player objects into an array with methods of creating a player instance and add to the array aswell as selecting a player object from the array with index value and also method to get all player object in the array. Create function to prompt user for their name and marker which then triggers a function and passes name and marker argument to add player to the player hub object and array.

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-document.body.textContent = 'hello'
+// create player factory functions
+function createPlayer(name, marker) {
+    let _marker = marker;
+
+    return {
+        name: name,
+        marker: marker,
+        getMarker: () => {
+            return _marker;
+        },
+        setMarker: function(marker) {
+            marker = marker;
+        },
+
+    }
+}
+
+//  object to create and store players from factory
+const playerHub = {
+playersArray: [],
+addPlayer: function(name, marker) {
+    const player = createPlayer(name, marker);
+  this.playersArray.push(player);  
+  
+},
+getPlayer: function(index)  {
+    return this.playersArray[index];
+},
+getAllPlayers: function() {
+    return this.playersArray;
+},
+
+};
+
+
+
+// playerHub.addPlayer('MK', 'O')
+
+// function to get player 1 info
+
+function promptUser() {
+    const nameInput = prompt('Name', '');
+    const markerInput = prompt('Marker', '');
+
+    addPlayerToPlayerhub(nameInput, markerInput)
+
+}
+function addPlayerToPlayerhub(name, marker) {
+    playerHub.addPlayer(name, marker)
+
+}
+promptUser();
