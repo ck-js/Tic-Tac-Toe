c4e03c8975920dcb6d186ff801f768dba145cff3
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-10-22 22:56:30 +0700

    Create function to prompt the user to make a move on the gameboard ranks and files. Add ranks and files array to player factory and methods to get and set their positions on the gameboard. We label game board a through c and 1 through 3. Create function for each player so that updating the properties with methods is clean.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// gameboard object 
+const gameboard = {
+    // squares are labelled a, b, c for columns and 1,2,3 for rows
+    ranksAndFiles: [],
+    marker: [],
+    
+    
+}
 // create player factory functions
 function createPlayer(name, marker) {
     let _marker = marker;
@@ -5,11 +13,18 @@ function createPlayer(name, marker) {
     return {
         name: name,
         marker: marker,
-        getMarker: () => {
-            return _marker;
+        ranksAndFiles: [],
+        getMarker: function() {
+            return this.marker;
         },
         setMarker: function(marker) {
             marker = marker;
+        },
+        setRanksAndFiles: function(position) {
+            this.ranksAndFiles.push(position);
+        },
+        getRanksAndFiles: function() {
+            return this.ranksAndFiles;
         },
 
     }
@@ -49,4 +64,21 @@ function addPlayerToPlayerhub(name, marker) {
     playerHub.addPlayer(name, marker)
 
 }
-promptUser();
+
+// prompt user for name and marker
+playerHub.addPlayer('Mark', 'X')
+playerHub.addPlayer('Spencer', 'O')
+
+// prompt player to  make a move on the gameboard
+function promptPlayer1ToMakeMove() {
+const input = prompt(`${playerHub.getPlayer(0).name} pick a square', '`);
+playerHub.getPlayer(0).setRanksAndFiles(input)
+
+};
+function promptPlayer2ToMakeMove() {
+    const input = prompt(`${playerHub.getPlayer(1).name} pick a square', '`);
+    playerHub.getPlayer(1).setRanksAndFiles(input)
+    
+    }
+promptPlayer1ToMakeMove()
+promptPlayer2ToMakeMove()
