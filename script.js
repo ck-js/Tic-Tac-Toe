5a2fb56d4fa1d45dca34e7320b3ae4d81dfd9e44
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-10-25 22:10:10 +0700

    Add condition checks to the gameboard add move function that takes the moves position and cehcks if the value is equal to a specific rank and file value that represents a position in our gameboard structure array that is a more true reflection of our gameboard  which consists of a parent array with 3 child arrays that have 3 empty strings as elements to represent the tic tac toe grid. If the moves position matches a string then it will replace an empty string with the moves marker.

--- script.js
+++ script.js
@@ -16,11 +16,55 @@
 const gameboard = (function() {
     // squares are labelled a, b, c for columns and 1,2,3 for rows
     let ranksAndFilesAndMarker = [];
-    
+    let gameboardStructure = [
+    ['','',''],
+    ['','',''],
+    ['','',''],
+    ];
+
     return {
     addMove: function(position,marker) {
 const move = addMove(position,marker)
-ranksAndFilesAndMarker.push(move)
+ranksAndFilesAndMarker.push(move);
+
+// check and assign marker to row a
+if (move.position === 'a1') {
+gameboardStructure[0].shift();
+gameboardStructure[0].unshift(move.marker)
+
+}
+if (move.position === 'a2') { 
+gameboardStructure[0].splice(1,1,move.marker);
+}
+if (move.position === 'a3') {
+    gameboardStructure[0][2] = move.marker;
+}
+
+// check and assign marker to row b
+if (move.position === 'b1') {
+    gameboardStructure[1][0] = move.marker;
+}
+if (move.position === 'b2') {
+    gameboardStructure[1][1] = move.marker;
+}
+if (move.position === 'b3') {
+    gameboardStructure[1][2] = move.marker;
+}
+
+// check and assign marker to row c
+if (move.position === 'c1') {
+    gameboardStructure[2][0] = move.marker;
+}
+if (move.position === 'c2') {
+    gameboardStructure[2][1] = move.marker;
+}
+if (move.position === 'c3') {
+    gameboardStructure[2][2] = move.marker;
+}
+
+
+
+
     },
     getSingleMove: function(index) {
 return ranksAndFilesAndMarker[index];
@@ -28,9 +72,13 @@ return ranksAndFilesAndMarker[index];
     getAllMoves: function() {
         return ranksAndFilesAndMarker;
     },
+    getGameboardStructure: function() {
+        return gameboardStructure;
+    }
     }
 })();
 
+console.log(gameboard.getGameboardStructure());
 // factory function for add move method in gameboard object
 function addMove(position,marker) {
 
@@ -68,11 +116,9 @@ function createPlayer(name, marker) {
 // module to store gameflow instances
 const Gameflow = (function() {
 
-
-
 return {
-    // function to get player 1 info
-    
+
+// takes input from prompt user function to add to playerhub module    
     addPlayerToPlayerHub: function(name, marker) {
         PlayerHub.addPlayer(name, marker)
     
@@ -85,6 +131,26 @@ return {
     this.addPlayerToPlayerHub(nameInput, markerInput)
 
 },
+// function to prompt user to make a move 
+ promptPlayer1ToMakeMove: function() {
+    const input = prompt(`${PlayerHub.getPlayer(0).name} pick a square', '`);
+    const marker = PlayerHub.getPlayer(0).getMarker();
+    
+    PlayerHub.getPlayer(0).setRanksAndFiles(input)
+    gameboard.addMove(input, marker)
+    },
+     promptPlayer2ToMakeMove: function() {
+        const input = prompt(`${PlayerHub.getPlayer(1).name} pick a square', '`);
+        const marker = PlayerHub.getPlayer(1).getMarker();
+    
+        PlayerHub.getPlayer(1).setRanksAndFiles(input)
+        gameboard.addMove(input, marker)
+        
+        
+        },
+
+
+
 
 
 }
@@ -132,29 +198,32 @@ return {
 // }
 
 // prompt user for name and marker
-// PlayerHub.addPlayer('Mark', 'X')
-// PlayerHub.addPlayer('Spencer', 'O')
+PlayerHub.addPlayer('Mark', 'X')
+PlayerHub.addPlayer('Spencer', 'O')
 
 // prompt player to  make a move on the gameboard
-function promptPlayer1ToMakeMove() {
-const input = prompt(`${PlayerHub.getPlayer(0).name} pick a square', '`);
-const marker = PlayerHub.getPlayer(0).getMarker();
+// function promptPlayer1ToMakeMove() {
+// const input = prompt(`${PlayerHub.getPlayer(0).name} pick a square', '`);
+// const marker = PlayerHub.getPlayer(0).getMarker();
 
-PlayerHub.getPlayer(0).setRanksAndFiles(input)
-gameboard.addMove(input, marker)
-};
-function promptPlayer2ToMakeMove() {
-    const input = prompt(`${PlayerHub.getPlayer(1).name} pick a square', '`);
-    const marker = PlayerHub.getPlayer(1).getMarker();
+// PlayerHub.getPlayer(0).setRanksAndFiles(input)
+// gameboard.addMove(input, marker)
+// };
+// function promptPlayer2ToMakeMove() {
+//     const input = prompt(`${PlayerHub.getPlayer(1).name} pick a square', '`);
+//     const marker = PlayerHub.getPlayer(1).getMarker();
 
-    PlayerHub.getPlayer(1).setRanksAndFiles(input)
-    gameboard.addMove(input, marker)
+//     PlayerHub.getPlayer(1).setRanksAndFiles(input)
+//     gameboard.addMove(input, marker)
     
     
-    }
+//     }
 
-Gameflow.promptUser()
-Gameflow.promptUser()
+    // call the user to input name and marker
+// Gameflow.promptUser()
+// Gameflow.promptUser()
 
 // promptPlayer1ToMakeMove()
 // promptPlayer2ToMakeMove()
+Gameflow.promptPlayer1ToMakeMove();
+Gameflow.promptPlayer2ToMakeMove()
